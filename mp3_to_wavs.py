65be7018f5bf97737fbd3a8aad974fd2d6f86dbd
gomezrondon <gomezrondon@gmail.com>
2021-07-12 06:49:53 -0600

    Adding chunkSizeMs

--- mp3_to_wavs.py
+++ mp3_to_wavs.py
@@ -6,7 +6,7 @@ from pydub import AudioSegment
 import sys
 
 
-def convert_mp3_to_wavs(file_path):
+def convert_mp3_to_wavs(file_path, chunk_length_ms = 50000):
     # create a directory to store the audio chunks.
     try:
         os.mkdir('audio_chunks')
@@ -17,18 +17,20 @@ def convert_mp3_to_wavs(file_path):
     sound.export("sound.wav", format="wav")
 
     myaudio = AudioSegment.from_file("sound.wav", "wav")
-    chunk_length_ms = 50000  # pydub calculates in millisec
+    # chunk_length_ms = 50000  # pydub calculates in millisec
     chunks = make_chunks(myaudio, chunk_length_ms)
 
     os.chdir('audio_chunks')
     # create each chunk
+    i = 1
     for i, chunk in enumerate(chunks):
         chunk_name = "chunk{0}.wav".format(i)
         print("exporting", chunk_name)
         chunk.export(chunk_name, format="wav")
+        i = i + 1
 
 if __name__ == '__main__':
     arg = sys.argv[1:]
     print(arg[0])
 
-    convert_mp3_to_wavs(arg[0]) #mp3 location
+    convert_mp3_to_wavs(arg[0], int(arg[1])) #mp3 location
